255c5dcff08d2270319b1c46dbc0ad5e469c3b58
Saam04 <SUMITBODDU_ds@ltce.in>
2025-03-27 02:54:52 +0530

    Code enhancement

--- utils/tmdb.py
+++ utils/tmdb.py
@@ -2,12 +2,10 @@ import requests
 import os
 from dotenv import load_dotenv
 
-# Load API key from .env
 load_dotenv()
 TMDB_API_KEY = os.getenv("TMDB_API_KEY")
 TMDB_BASE_URL = "https://api.themoviedb.org/3"
 
-# Ensure this function exists
 def fetch_movie_details(title):
     url = f"{TMDB_BASE_URL}/search/movie"
     params = {"api_key": TMDB_API_KEY, "query": title}
@@ -16,7 +14,7 @@ def fetch_movie_details(title):
     data = response.json()
     
     if "results" in data and data["results"]:
-        movie = data["results"][0]  # Get the first search result
+        movie = data["results"][0]  
         return {
             "title": movie["title"],
             "year": int(movie["release_date"].split("-")[0]) if movie.get("release_date") else None,
@@ -24,4 +22,4 @@ def fetch_movie_details(title):
             "rating": movie.get("vote_average", 0)
         }
     
-    return None  # Return None if no movie is found
+    return None
