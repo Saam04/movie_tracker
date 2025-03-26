33b2d6b6718948f39208cb959da5d2af38912fa6
Saam04 <SUMITBODDU_ds@ltce.in>
2025-03-27 02:53:49 +0530

    added DBeaver to make it more versatile

--- config/db.py
+++ config/db.py
@@ -3,12 +3,10 @@ from flask_sqlalchemy import SQLAlchemy
 import os
 from dotenv import load_dotenv
 
-# Load environment variables
 load_dotenv()
 
 app = Flask(__name__)
 
-# ✅ Correct Database Configuration
 app.config['SQLALCHEMY_DATABASE_URI'] = "mysql+pymysql://root:8080@localhost/movie_tracker_db"
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False  
 
