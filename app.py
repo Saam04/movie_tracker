3ccb1200b3eec70b224da3b4171a825faa736790
Saam04 <SUMITBODDU_ds@ltce.in>
2025-03-27 02:52:56 +0530

    Added diff DB to make it more efficient

--- app.py
+++ app.py
@@ -2,10 +2,8 @@ from flask import Flask
 from config.db import app, db
 from routes.movie_routes import movie_routes
 
-# Register Blueprints
 app.register_blueprint(movie_routes)
 
-# Initialize Database (Fixes missing table issue)
 with app.app_context():
     db.create_all()
 
